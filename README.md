ba8d8b41cf16713ecf376c64ec50e24b7db13c1d
Carlos Dourado <cadkrav10@gmail.com>
2021-03-10 09:29:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+cd dynamicalaccordian
+
+npm start
+
+## instala :
+
+npm i react-bootstrap-table2-paginator react-bootstrap-table-next axios
+
+npm i react-bootstrap axios
+
+
+## no index.html 
+
+colocar os links do bootstrap
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React,{useState, useEffect} from 'react';
+import './App.css';
+import { Accordion, Card } from 'react-bootstrap';
+import axios from 'axios';
+
+const App = () => {
+
+  const [teamData, setTeamData] = useState([])
+  
+
+  const nbaData = async () => {
+    const response = await axios.get("https://www.balldontlie.io/api/v1/teams/");
+
+    setTeamData(response.data.data);
+  }
+
+  const renderAccordion = (team, index) => {
+    return (
+      <Accordion key={index}>
+        <Card>
+          <Accordion.Toggle  as={Card.Header} eventKey={team}>
+            {team.city}
+            </Accordion.Toggle>
+          <Accordion.Collapse eventKey={team}>
+              <Card.Body>
+              <ul>
+                <li>{team.abbreviation}</li>
+                <li>{team.city}</li>
+                <li>{team.conference}</li>
+                <li>{team.division}</li>
+                <li>{team.full_name}</li>
+              </ul>
+              </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        
+      </Accordion>
+    );
+  };
+
+  useEffect(() => {
+    nbaData();
+  }, []);
+
+  return (
+    <div className="App">
+     {teamData.map(renderAccordion)}
+    </div>
+  );
+}
+
+export default App;
